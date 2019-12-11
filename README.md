cb847cb771ec1f2aca4f985a910f340e64fc34bd
Robert Loquinario <lokinerio@gmail.com>
2019-12-10 23:19:13 -0800

    using tweepy, boutta unfollow ppl soon

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # stayloyal
 twitter api bot to unfollow people that unfollow me
+
+# TODO
+
+- write to csv so i have history of followers
+- unfollow people 
---
+++ main.py
@@ -0,0 +1,38 @@
+import tweepy
+import csv
+import time
+import webbrowser
+# Credentials
+import credentials as c
+def main():
+    auth = tweepy.OAuthHandler(c.CONSUMER_KEY, c.CONSUMER_SECRET)
+    auth.set_access_token(c.ACCESS_TOKEN, c.ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
+
+    # values
+    followers = list()
+    friends = list()
+
+    # get followers
+    f = tweepy.Cursor(api.followers,count=200).items()
+    for u in f:
+        followers.append(u.screen_name)
+    print(len(followers))
+
+    users = tweepy.Cursor(api.friends, count = 200).items()
+    for u in users:
+        if (((u.followers_count < 1000) and (u.followers_count != 0))and not (u.verified)):
+            friends.append(u.screen_name)
+            #print(u.screen_name,u.followers_count)
+        else:
+            pass
+
+
+    print(len(friends))
+
+    # check whos a snake
+    for friend in friends:
+        if friend not in followers:
+            print(friend)
+if __name__ == "__main__":
+    main()
