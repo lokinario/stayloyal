42b69900ca89cbd32878e1c6a97519b9c5df621d
Robert Loquinario <lokinerio@gmail.com>
2019-12-11 13:27:05 -0800

    added the ability to use command line arguments to tweet instead of having to get prompted

--- status_update.py
+++ status_update.py
@@ -1,14 +1,28 @@
 import tweepy
-import time
+import sys
 # Credentials
 import credentials as c
+
+
 def main():
+    if (len(sys.argv) < 2):
+        tweet = input("What's on your mind? ")
+    else:
+        # p = 0
+        # while (p < len(sys.argv) - 1):
+        #     print("parameter %i: %s" % (p, sys.argv[p])
+        #     p = p + 1
+        #     # tweet += sys.argv[p]
+        tweet = ''
+        for i in sys.argv[1:]:
+            tweet += i + ' '
+
     auth = tweepy.OAuthHandler(c.CONSUMER_KEY, c.CONSUMER_SECRET)
     auth.set_access_token(c.ACCESS_TOKEN, c.ACCESS_TOKEN_SECRET)
     api = tweepy.API(auth)
 
-    tweet = input("What's on your mind? ")
     api.update_status(tweet)
+    # print(tweet)
 
 
 if __name__ == "__main__":
